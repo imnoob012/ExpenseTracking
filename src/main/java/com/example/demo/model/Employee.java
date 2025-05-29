92e2ddae190c140bae2fb37bb196fd095c1d8136
imnoob012 <ritsuki28@icloud.com>
2025-05-29 10:13:52 +0900

    自分の中でdtoとentityとかがややこしくなったので"model"パッケージ作成し、そこに全て入れるように変更しました

---
+++ src/main/java/com/example/demo/model/Employee.java
@@ -0,0 +1,12 @@
+package com.example.demo.model;
+
+import lombok.Data;
+
+@Data
+public class Employee {
+	private int id;
+	private String name;
+	private String mail;
+	private String deletedate;
+	private String updatedate;
+}
